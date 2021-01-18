0f82670d7c44da175359cb468f8cd2159c517be1
Megan Lavallee <megan.lavallee@yahoo.com>
2021-01-17 20:28:50 -0700

    database, service worker, manifest, updating server

---
+++ public/db.js
@@ -0,0 +1,53 @@
+let db;
+const budget = indexedDB.open("budget", 1);
+
+budget.onupgradeneeded = function (e) {
+  const db = e.target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
+};
+
+budget.onsuccess = function (e) {
+  db = e.target.result;
+  if (navigator.onLine) {
+    database();
+  }
+};
+
+budget.onerror = function (e) {
+  console.log("Woops! " + e.target.errorCode);
+};
+
+function saveRecord(record) {
+  const transaction = db.transaction(["pending"], "readwrite");
+
+  const store = transaction.objectStore("pending");
+
+  store.add(record);
+}
+
+function database() {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+  const get = store.get();
+
+  get.onsuccess = function () {
+    if (get.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(get.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then(() => {
+          const transaction = db.transaction(["pending"], "readwrite");
+          const store = transaction.objectStore("pending");
+          store.clear();
+        });
+    }
+  };
+}
+
+window.addEventListener("online", database);
